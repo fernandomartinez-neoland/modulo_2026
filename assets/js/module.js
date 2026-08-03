0691234ebbf1967c7e3e6b12b52242c90bf7890b
altrios <famartinez.ti@gmail.com>
2026-08-03 10:14:36 +0200

    mas cosas con funciones

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,3 +1,34 @@
-import {consola} from './scripts.js'
+// importacion nombrada
+// se le dice asi porque lleva llaves y esta tiene que escribirse de manera literal como fue escrita la funcion
+import { consola, retorno, suma, flecha } from "./scripts.js";
 
-consola()
+// importación por defecto
+// cuando hacemos importacion por defecto, nosotros podemos renombrar en la importacion el nombre de la funcion por defecto del fichero
+// esto generalmente se hace cuando existe mas de 1 funcion con el mismo nombre en distintos ficheros y deben ser importados en el modulo
+import porDefecto from "./scripts.js";
+
+consola();
+porDefecto();
+
+// cuando usamos una funcion que retorna valores, generalmente la tratamos como a una variable y la asignamos a otra, desde ese momento la nueva variable tendra como valor lo que retorna la funcion que le fue asignada
+const valor_retorno = retorno();
+
+console.log(valor_retorno);
+
+// cuando llamamos a una funcion podemos (O NO) enviarle parametros, esto depende de la declaracion
+// si la funcion espera valores, es recomendable mandarle la cantidad de valores(parametros) que esta necesite
+
+// se le deben enviar la misma cantidad de parametros, mismo tipo de parametros y en el exacto orden en el que la funcion lo solicita
+
+// los parametros deben estar separados por coma (,)
+
+const sumar = suma(4, 5);
+console.log("suma: ", sumar);
+
+
+flecha();
+
+
+
+
+// la cascada puede ser saltada con funciones y clases (luego veremos clases), esto quiere decir que si necesitas llamar a una funcion dentro del mismo fichero, puedes hacerlo antes de la linea de declaracion, ya que la cascada no lee funciones, solo lee el llamado de las mismas y es la variable o el mismo llamado quien se encarga de buscarlo
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,38 @@
-
+// las funciones nombradas deben ser llamadas desde el modulo escritas de la manera literal de como fueron declaradas
 export function consola(){
-    console.log("holi")
+    console.log("consola")
+}
+
+// solo puede existir 1 funcion por defecto en cada fichero
+// no es lo recurente pero se usa cuando necesitamos crear 1 funcion con el mismo nombre en mas de 1 fichero por proyecto
+
+export default function defecto(){
+    console.log("funcion por defecto")
+}
+
+export function retorno(){
+// esto se usa generalmente cuando al llamar a una funcion, necesitamos que esta RETORNE un valor
+    return "retorno"
+}
+
+// las funciones pueden recibir entre 0 y X parametros
+// los parametros son variables vacias hasta que en el llamado se envian valores
+// los parametros reciben los valores que se le envian a la funcion durante el llamado en EXACTAMENTE  el mismo orden
+// los parametros reciben CUALQUIER tipo de valor (string, int, boolean, etc)
+export function suma(numA, numB){
+    
+    // una vez que los paramtros perciben un valor, estos pasan a ser usados segun como la funcion necesite (no hay regla especifica sobre esto)
+
+    // toda variable y parametro que sea declarado dentro de la funcion SOLO EXISTE dentro de la funcion
+    // no se puede declarar una variable dentro de la funcion para luego ser usada fuera de la funcion
+
+    // se puede declarar una variable dentro de la funcion y luego fuera de la funcion, ante al ejecucion son 2 variables distintas
+
+    // si declaras una variable fuera de la funcion, puedes usarla dentro de la funcion (simpre y cuando esta variable no fuese declarada previamente dentro de otra funcion)
+    return numA+numB
+}
+
+
+export const flecha =()=>{
+    console.log("funcion de flecha")
 }
